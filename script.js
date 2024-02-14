1dec854e302eaacccfa75dede490669d150e9252
VitaliPri <vitaliy.prymak@cix.csi.cuny.edu>
2024-02-14 10:25:35 -0500

    animated navigation

--- script.js
+++ script.js
@@ -5,24 +5,35 @@ const nav2 = document.getElementById("nav-2");
 const nav3 = document.getElementById("nav-3");
 const nav4 = document.getElementById("nav-4");
 const nav5 = document.getElementById("nav-5");
+const navItems = [nav1, nav2, nav3, nav4, nav5];
+
+// Control navigation
+function navAnimation(direction1, direction2) {
+  navItems.forEach((nav, i) => {
+    nav.classList.replace(
+      `slide-${direction1}-${i + 1}`,
+      `slide-${direction2}-${i + 1}`
+    );
+  });
+}
 
 function toggleNav() {
   // Toggle: Open/Close
   menuBars.classList.toggle("change");
   overlay.classList.toggle("overlay-active");
   if (overlay.classList.contains("overlay-active")) {
-    overlay.classList.remove("overlay-slide-left");
-    overlay.classList.add("overlay-slide-right");
+    overlay.classList.replace("overlay-slide-left", "overlay-slide-right");
+    // Animate-in
+    navAnimation("out", "in");
   } else {
-    overlay.classList.remove("overlay-slide-right");
-    overlay.classList.add("overlay-slide-left");
+    overlay.classList.replace("overlay-slide-right", "overlay-slide-left");
+    // Animate-out
+    navAnimation("in", "out");
   }
 }
 
 // Event Listeners
 menuBars.addEventListener("click", toggleNav);
-nav1.addEventListener("click", toggleNav);
-nav2.addEventListener("click", toggleNav);
-nav3.addEventListener("click", toggleNav);
-nav4.addEventListener("click", toggleNav);
-nav5.addEventListener("click", toggleNav);
+navItems.forEach((nav) => {
+  nav.addEventListener("click", toggleNav);
+});
